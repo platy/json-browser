7bdc39172645a1c1b09eb9ca72e3a48aeedb0ef2
mike <mpbush@gmail.com>
2013-04-24 23:22:17 +0100

    Some reformatting on background.js

--- src/background.js
+++ src/background.js
@@ -1,37 +1,35 @@
+function getHeader(headers, headerName) {
+  var i;
+  for (i = 0; i < headers.length; i++) {
+    if (headers[i].name.toLowerCase() === headerName.toLowerCase()) {
+      return headers[i].value;
+    }
+  }
+  console.error("Header " + headerName + " not found");
+}
+
 function schemaDescriptionForResponse(details) {
-  var contentTypeHeader = getHeader(details.responseHeaders, "content-type")
+  var contentTypeHeader = getHeader(details.responseHeaders, "content-type");
   var profile = contentTypeHeader.split(";")[1].split("profile=")[1];
   return {schemaUrl: profile};
 }
 
 function onJsonPage(details, retry) {
   console.log("JSON page " + details.tabId);
-  chrome.tabs.sendMessage(details.tabId, 
-      schemaDescriptionForResponse(details),
-      function(response) {
-        if (response === undefined) {
-          console.error("Message failed - " + chrome.runtime.lastError.message + " - " + retry + " retries remaining.");
-          if (retry > 0)
-            onJsonPage(details, retry - 1);
-        } else
-          console.log("Content script instructed to reload with schema");
+  chrome.tabs.sendMessage(
+    details.tabId,
+    schemaDescriptionForResponse(details),
+    function (response) {
+      if (response === undefined) {
+        console.error("Message failed - " + chrome.runtime.lastError.message + " - " + retry + " retries remaining.");
+        if (retry > 0) {
+          onJsonPage(details, retry - 1);
+        }
+      } else {
+        console.log("Content script instructed to reload with schema");
       }
-    );
-}
-
-function onCompleted(details) {
-  console.log("completed");
-  var headers = details.responseHeaders;
-  var contentTypeHeader = getHeader(headers, "content-type")
-  if (contentTypeIsJson(contentTypeHeader))
-    onJsonPage(details, 1);
-}
-
-function getHeader(headers, headerName) {
-  for (var i=0; i<headers.length; i++)
-    if (headers[i].name.toLowerCase()==headerName.toLowerCase())
-      return headers[i].value;
-  console.error("Header " + headerName + " not found");
+    }
+  );
 }
 
 function contentTypeIsJson(contentTypeHeader) {
@@ -43,16 +41,29 @@ function contentTypeIsJson(contentTypeHeader) {
   return contentTypeHeader.split(";")[0].trim() === "application/json";
 }
 
+function onCompleted(details) {
+  console.log("completed");
+  var headers = details.responseHeaders;
+  var contentTypeHeader = getHeader(headers, "content-type");
+  if (contentTypeIsJson(contentTypeHeader)) {
+    onJsonPage(details, 1);
+  }
+}
+
 var filter = {
   urls: ["<all_urls>"],
   types: ["main_frame"]
 };
 
 chrome.webRequest.onCompleted.addListener(
-    onCompleted, filter, ["responseHeaders"]);
+  onCompleted,
+  filter,
+  ["responseHeaders"]
+);
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
+  function (request, sender, sendResponse) {
     chrome.pageAction.show(sender.tab.id);
     sendResponse();
-  });
+  }
+);
